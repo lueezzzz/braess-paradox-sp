8f15ea5700aaff2a69fe7c12e295f2c96b921a97
lueezzzz <rianmabait45@gmail.com>
2026-03-04 21:46:02 +0800

    use csv for visualization

--- visualizer.py
+++ visualizer.py
@@ -1,6 +1,35 @@
+import csv
 import osmnx as ox
 import networkx as nx
-from city.final_network import intersections, roads
+
+intersections = []
+with open("data/intersections.csv", mode="r", encoding="utf-8") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        intersections.append(
+            {
+                "id": row["id"],
+                "y": float(row["y"]),
+                "x": float(row["x"]),
+                "name": row["name"],
+            }
+        )
+
+roads = []
+with open("data/roads.csv", mode="r", encoding="utf-8") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        roads.append(
+            {
+                "A": row["A"],
+                "B": row["B"],
+                "distance": float(row["distance"]),
+                "lanes": int(row["lanes"]),
+                "oneway": row["oneway"].strip().lower() == "true",
+                "name": row["name"],
+            }
+        )
+
 
 G = nx.MultiDiGraph()
 G.graph["crs"] = "EPSG:4326"
@@ -8,13 +37,12 @@ G.graph["crs"] = "EPSG:4326"
 for node in intersections:
     G.add_node(node["id"], x=node["x"], y=node["y"])
 
-
 for road in roads:
     G.add_edge(road["A"], road["B"], length=road["distance"])
     if not road["oneway"]:
         G.add_edge(road["B"], road["A"], length=road["distance"])
 
-molo_network = ox.plot_graph(G, show=True, close=False, edge_color="w")
+iloilo_network = ox.plot_graph(G, show=True, close=False, edge_color="w")
 
-# plt.savefig("molo_network_map.png", dpi=300, bbox_inches="tight")
-# print("Plot saved as molo_network_map.png")
+# plt.savefig("iloilo_network_map.png", dpi=300, bbox_inches="tight")
+# print("Plot saved as iloilo_network_map.png")
